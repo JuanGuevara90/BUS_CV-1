e5bfcae43773300af26b079747348fa1c79b1fc8
Jessica Quimbia <estefaniaql99@gmail.com>
2022-08-04 16:56:04 -0500

    Se añadio una base de datos, una tabla padre llamada registro pasajeros y una tabla hijo  bus

--- Registro_pasajeros.py
+++ Registro_pasajeros.py
@@ -42,22 +42,16 @@ def main():
     database = r"C:\Users\Jessica\Desktop\Face\DB SQLite\faceDatabase.db"
  
 
-    sql_create_projects_table = """ CREATE TABLE IF NOT EXISTS projects (
-                                        id integer PRIMARY KEY,
-                                        name text NOT NULL,
-                                        begin_date text,
-                                        end_date text
+    sql_create_projects_table = """ CREATE TABLE IF NOT EXISTS Registro_Pasajeros (
+                                        Total_Pasajeros integer,
+                                        date text
                                     ); """
 
-    sql_create_tasks_table = """CREATE TABLE IF NOT EXISTS tasks (
-                                    id integer PRIMARY KEY,
-                                    name text NOT NULL,
-                                    priority integer,
-                                    status_id integer NOT NULL,
-                                    project_id integer NOT NULL,
-                                    begin_date text NOT NULL,
-                                    end_date text NOT NULL,
-                                    FOREIGN KEY (project_id) REFERENCES projects (id)
+    sql_create_tasks_table = """CREATE TABLE IF NOT EXISTS Bus (
+                                    origen text,
+                                    destino text,
+                                    aforo interger
+                                
                                 );"""
 
     # create a database connection
