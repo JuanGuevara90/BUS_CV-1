0bb09d66d9ff2d2a574ccdc485330a6bd3e7f331
Juan Pablo Guevara <guevara_juan1990@hotmail.com>
2022-08-13 10:44:44 -0500

    feat: :sparkles: Add serial

--- src/raspberry/Controlador.py
+++ src/raspberry/Controlador.py
@@ -1,6 +1,7 @@
 from .utiles.getDateCurrent import getDate_Current
 from .database.operaciones import existeRegistrosFechaActual,ingresarRegistroPasajeros,disponibilidadBus,actualizarRegistroSuma,actualizarRegistroResta,validateLeft,getDatosActuales,getRoutes
 from .database.conexion import create_connection,main,isSqlite3Db
+from .serial.sendData import sendDatabySerial
 
 def controladorIngreso():
     if( isSqlite3Db() ): 
@@ -12,13 +13,16 @@ def controladorIngreso():
         if( disponibilidadBus( conn , dateCurrent ) ):
             actualizarRegistroSuma( conn , dateCurrent )
             """ Actualizar y enviar al arduino"""
+            #sendDatabySerial("A")
             print("Ingreso un pasajero")
         else:
             """ Enviar al arduino bloqueado la puerta """
-            print("Pruerta Bloqueada")
+            #sendDatabySerial("B")
+            print("Puerta Bloqueada")
     else:
         """ Enviar al arduino """
         ingresarRegistroPasajeros( conn , dateCurrent )
+        #sendDatabySerial("A")
         print("Ingreso al inicio del dia")
     print(getRoutes(conn))
     print(getDatosActuales(conn,dateCurrent))
@@ -31,6 +35,7 @@ def controladorSalida():
             if( validateLeft( conn , dateCurrent )):
                 actualizarRegistroResta( conn , dateCurrent )
                 """ Actualizar y enviar al arduino"""
+                #sendDatabySerial("A")
                 print("Ingreso un pasajero")
             else:
                 print("Enviar al arduino ")
