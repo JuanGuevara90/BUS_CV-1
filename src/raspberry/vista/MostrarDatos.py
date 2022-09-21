bf94deb499637f0cf12b3c09ded1d411177e7d34
Quimbia <estefaniaql99@gmail.com>
2022-09-21 16:59:10 -0500

    update

--- src/raspberry/vista/MostrarDatos.py
+++ src/raspberry/vista/MostrarDatos.py
@@ -1,4 +1,5 @@
 import serial, time
+#import RPi.GPIO as GPIO
 from raspberry.controlador import controladorDatos
 
 def connection():
@@ -24,9 +25,18 @@ def estado(d1):
     puerto = connection()
     dato=d1     
     if dato>=0 and dato<5:
-       puerto.write(b"t6.bco=GREEN"'\xFF\xFF\xFF')
+       puerto.write(b"t6.bco=GREEN"+b'\xFF\xFF\xFF')
     if dato>=5 and dato<10:
-       puerto.write(b"t6.bco=64512"'\xFF\xFF\xFF')
+       puerto.write(b"t6.bco=64512"+b'\xFF\xFF\xFF')
     if dato>=10 and dato<=15:
-       puerto.write(b"t6.bco=RED"'\xFF\xFF\xFF')
+       puerto.write(b"t6.bco=RED"+b'\xFF\xFF\xFF')
     
+"""def ShutdownButton(self):
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(8,GPIO IN, pull_up_down=GPIO.PUD_UP)
+    def ButtonApagar():
+        while True:
+            if GPIO.input(8)==False:
+                print("La raspberry Pi se apagará")
+                subprocess.call(['sudo','shutdown','now'])"""
