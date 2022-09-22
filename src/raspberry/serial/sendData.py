beea7b7a2710327c0b57004b34289001cf568285
CristhianC96 <cristhian.corral1996@gmail.com>
2022-09-22 10:15:28 -0500

    se aumento .env para sensores en controlador ; se aumento la funcion sensorica en la quye se llama al sensor y generamos la funcion control sensor para el .py del controlador

--- src/raspberry/serial/sendData.py
+++ src/raspberry/serial/sendData.py
@@ -11,40 +11,18 @@ def sendDatabySerial(msg):
     load_dotenv(dotenv_path)
     FRECUENCIA = os.environ.get("FRECUENCIA")
     DISPOSITIVO = os.environ.get("DISPOSITIVO")
-    ser=serial.Serial(''+DISPOSITIVO, FRECUENCIA, timeout=1)
-    ser.write(b''+msg)
+    ser=serial.Serial(''+DISPOSITIVO, FRECUENCIA)
+    ser.write(msg.encode("utf-8"))
     ser.close()
 
 
 def arduino (mnsj):
-  arduino = serial.Serial("COM8", 9600)
+  arduino = serial.Serial("COM5", 9600)
   time.sleep(2)
   arduino.write(mnsj.encode("utf-8"))
   arduino.close()
 
 
 
-def ardrecibe():
 
-  hw_sensor = serial.Serial(port='COM6', baudrate=115200, timeout=1, write_timeout=1)
-
-  if __name__ == '__main__':
-    while True:
-        hw_sensor.write('getValue'.encode('utf-8'))
-        time.sleep(1)
-        try:
-            raw_string_b = hw_sensor.readline()
-            raw_string_s = raw_string_b.decode('utf-8')
-            if(raw_string_s.index("}")>=0 and raw_string_s.index("{")==0):
-                raw_string_s = raw_string_s[0:raw_string_s.index("}")+1]
-                raw_string_j = json.loads(raw_string_s)
-                print(raw_string_j)
-                print(raw_string_j["Sensor_id"])
-                print(raw_string_j["Value"])
-                return raw_string_j
-            else:
-                print("error/ no } found.")
-        except:
-            print("Exception occurred, somthing wrong...")
-  hw_sensor.close()
 
