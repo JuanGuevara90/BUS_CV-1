60f2a69d417a6e5c2020d8d2fc75b6fd435b8a9d
Quimbia <estefaniaql99@gmail.com>
2022-09-22 15:50:15 -0500

    Se cambió en Mostrar Datos (text por un number)

--- src/raspberry/vista/MostrarDatos.py
+++ src/raspberry/vista/MostrarDatos.py
@@ -18,8 +18,8 @@ def agregar():
     d2=(datospantalla[2])
     lblpi=d1
     lblps=d2
-    puerto.write(b't4'+str(lblpi).encode()+b' '+b'\xFF\xFF\xFF')
-    puerto.write(b't4'+str(lblps).encode()+b' '+b'\xFF\xFF\xFF')
+    puerto.write(b'n00.val='+str(lblpi).encode()+b'\xFF\xFF\xFF')
+    puerto.write(b'n1.val='+str(lblps).encode()+b'\xFF\xFF\xFF')
 
 def estado(d1):
     puerto = connection()
